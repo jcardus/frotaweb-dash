f7c63a9ef1784996be6c8abac62cf6999f2d09b6
jcardus <joaquim_cardeira@hotmail.com>
2024-11-03 03:55:38 +0000

    apex charts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="grid grid-cols-4 gap-4">
+    <div>01</div>
+    <!-- ... -->
+    <div>09</div>
+</div>
