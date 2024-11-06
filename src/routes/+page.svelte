e83495a3675312d72b12f94d6b286935d277dd33
jcardus <joaquim_cardeira@hotmail.com>
2024-11-06 17:16:25 +0000

    new donut 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,18 +21,17 @@
         "#C70039", // Crimson
         "#900C3F"]  // Dark Red
 
-    import DeviceStatesDonut from "$lib/DeviceStatesDonut.svelte";
     import DeviceTotalKms from "$lib/DeviceTotalKms.svelte";
     import DeviceLastUpdate from "$lib/DeviceLastUpdate.svelte";
     import DeviceTotalHours from "$lib/DeviceTotalHours.svelte";
     import DeviceAlarm from "$lib/DeviceAlarm.svelte";
+    import DeviceIgnitionDonut from "$lib/DeviceIgnitionDonut.svelte";
     let {data} = $props()
     let {devices, positions} = data
 </script>
 <div class="grid grid-cols-3 gap-5 p-5" >
-    <DeviceStatesDonut {devices}></DeviceStatesDonut>
-    <!--DeviceIgnitionDonut {positions} {devices}></DeviceIgnitionDonut-->
     <DeviceStatesGroupedDonut {devices}></DeviceStatesGroupedDonut>
+    <DeviceIgnitionDonut {positions} {devices}></DeviceIgnitionDonut>
     <DeviceAlarm {positions} {devices}></DeviceAlarm>
     <DeviceTotalKms {positions} {devices}></DeviceTotalKms>
     <DeviceTotalHours {positions} {devices}></DeviceTotalHours>
