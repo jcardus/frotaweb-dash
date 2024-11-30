01f9c058dca7db82523adb3d48c14b8e5f1bdea1
jcardus <joaquim_cardeira@hotmail.com>
2024-11-30 00:56:57 +0000

    version

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
-	import '../app.css';
-	let { children } = $props();
+    import '../app.css';
+    let {children} = $props();
+    const version = import.meta.env.PACKAGE_VERSION;
 </script>
+
+<svelte:head>
+    <title>dash {version}</title>
+</svelte:head>
 
 {@render children()}
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { readFileSync } from 'fs';
+const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
 
 export default defineConfig({
 	plugins: [sveltekit()],
@@ -8,5 +10,8 @@ export default defineConfig({
 			'/api/socket': 'ws://gps.rastreosat.com.br',
 			'/api': 'http://gps.rastreosat.com.br',
 		},
-	}
+	},
+	define: {
+		'import.meta.env.PACKAGE_VERSION': JSON.stringify(pkg.version),
+	},
 });
