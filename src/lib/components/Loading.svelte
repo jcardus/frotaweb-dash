7849aca326f60d9ebdc73265c980e43fd5c17e45
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2025-11-01 20:44:51 +0000

    allow resize during fetch

--- src/lib/components/Loading.svelte
+++ src/lib/components/Loading.svelte
@@ -1,6 +1,7 @@
 <script>
     import {onDestroy} from "svelte";
     import {loadingDevice} from "$lib/store.js";
+    import palette from "$lib/palette.js";
 
     let _loadingDevice = $state('');
 
@@ -10,7 +11,7 @@
 
     onDestroy(unsubscribe);
 </script>
-<div class="absolute inset-0 p-2 flex flex-col items-center justify-center bg-gray-200 bg-opacity-25 z-10">
+<div class="absolute h-32 inset-0 p-2 flex flex-col items-center justify-center bg-gray-200 bg-opacity-0 z-10">
     <div class="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-600"></div>
-    <div class="w-full mt-4 text-center text-gray-700 font-medium text-sm">{_loadingDevice}</div>
+    <div style="{`color: ${palette(undefined, undefined).secondary.main}`}" class="w-full mt-4 text-center font-medium text-sm">{_loadingDevice}</div>
 </div>
